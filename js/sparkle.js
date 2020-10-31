60196411c93f0a3ad5bd7b18796d2dedfdaac65f
Kishor <cyberkishor@gmail.com>
2020-10-31 12:19:46 +0545

    path change

--- js/sparkle.js
+++ js/sparkle.js
@@ -3,123 +3,123 @@ var documentList = [
     {
         "title" : "Construction Light",
         "description" : "Construction Light is a user-friendly, feature-rich, intuitive, creative, powerful, impressive engaging and dynamic, fully customizable responsive.",
-        "link"      : "http://docs.sparklewpthemes.com/constructionlight/",
+        "link"      : "https://docs.sparklewpthemes.com/constructionlight/",
         "image"     : 'https://i0.wp.com/themes.svn.wordpress.org/construction-light/1.0.6/screenshot.png?w=572&strip=all'
     },
     {
         "title" : "Blogger Buzz",
         "description" : "Blogger Buzz is a user-friendly, feature-rich simple and clean, modern, stylish and beautiful fully customizable responsive free WordPress blog theme",
-        "link"      : "http://docs.sparklewpthemes.com/bloggerbuzz/",
-        "image"     : 'http://docs.sparklewpthemes.com/bloggerbuzz/assets/images/blogger-buzz.png'
+        "link"      : "https://docs.sparklewpthemes.com/bloggerbuzz/",
+        "image"     : 'https://docs.sparklewpthemes.com/bloggerbuzz/assets/images/blogger-buzz.png'
     },
     {
         "title" : "Blogger Buzz Pro",
         "description" : "Blogger Buzz Pro is a user-friendly, feature-rich simple and clean, modern, stylish and beautiful fully customizable responsive free WordPress blog theme",
-        "link"      : "http://docs.sparklewpthemes.com/bloggerbuzzpro/",
-        "image"     : 'http://docs.sparklewpthemes.com/bloggerbuzzpro/assets/images/blogger-buzz.png'
+        "link"      : "https://docs.sparklewpthemes.com/bloggerbuzzpro/",
+        "image"     : 'https://docs.sparklewpthemes.com/bloggerbuzzpro/assets/images/blogger-buzz.png'
     },
     
     {
         "title" : "Education Xpert",
         "description" : "Educenter Xpert is an ideal Education Free WordPress Theme. It's a simple, clean, elegant, user-friendly, impressive, engaging, and dynamic theme",
-        "link"      : "http://docs.sparklewpthemes.com/educenter/",
-        "image"     : 'http://docs.sparklewpthemes.com/educenter/assets/images/educenter-cover-bg.jpg'
+        "link"      : "https://docs.sparklewpthemes.com/educenter/",
+        "image"     : 'https://docs.sparklewpthemes.com/educenter/assets/images/educenter-cover-bg.jpg'
     },
     {
         "title" : "Education Center",
         "description" : "Educenter is an ideal Education Free WordPress Theme. It's a simple, clean, elegant, user-friendly, impressive, engaging, and dynamic theme",
-        "link"      : "http://docs.sparklewpthemes.com/educenter/",
-        "image"     : 'http://docs.sparklewpthemes.com/educenter/assets/images/educenter-cover-bg.jpg'
+        "link"      : "https://docs.sparklewpthemes.com/educenter/",
+        "image"     : 'https://docs.sparklewpthemes.com/educenter/assets/images/educenter-cover-bg.jpg'
     },
     {
         "title" : "Education Center Pro",
         "description" : "Educenter Center Pro is an ideal Education Free WordPress Theme. It's a simple, clean, elegant, user-friendly, impressive, engaging, and dynamic theme",
-        "link"      : "http://docs.sparklewpthemes.com/educenterpro/",
-        "image"     : 'http://docs.sparklewpthemes.com/educenter/assets/images/educenter-cover-bg.jpg'
+        "link"      : "https://docs.sparklewpthemes.com/educenterpro/",
+        "image"     : 'https://docs.sparklewpthemes.com/educenter/assets/images/educenter-cover-bg.jpg'
     },
     {
         "title" : "Fitness Park",
         "description" : "Fitness Park is a user-friendly and feature-rich, intuitive and creative, powerful and impressive engaging and dynamic, tech-savvy and good looking theme",
-        "link"      : "http://docs.sparklewpthemes.com/fitnesspark/",
+        "link"      : "https://docs.sparklewpthemes.com/fitnesspark/",
         "image"     : 'https://i0.wp.com/themes.svn.wordpress.org/fitness-park/1.0.1/screenshot.png?w=572&strip=all'
     },
     {
         "title" : "Fitness Park Pro",
         "description" : "Fitness Park Pro is a user-friendly and feature-rich, intuitive and creative, powerful and impressive engaging and dynamic, tech-savvy and good looking theme",
-        "link"      : "http://docs.sparklewpthemes.com/fitnessparkpro/",
+        "link"      : "https://docs.sparklewpthemes.com/fitnessparkpro/",
         "image"     : 'https://i0.wp.com/themes.svn.wordpress.org/fitness-park/1.0.1/screenshot.png?w=572&strip=all'
     },
     {
         "title" : "Medical Heed",
         "description" : "Medical Heed is clean & beautiful free multipurpose medical WordPress theme, This theme well suited for a hospital, general clinics, nursing home etc theme",
-        "link"      : "http://docs.sparklewpthemes.com/medicalheed/",
+        "link"      : "https://docs.sparklewpthemes.com/medicalheed/",
         "image"     : 'https://i0.wp.com/themes.svn.wordpress.org/medical-heed/1.0.2/screenshot.png?w=572&strip=all'
     },
     {
         "title" : "Medical Heed Pro",
         "description" : "Medical Heed Pro is clean & beautiful free multipurpose medical WordPress theme, This theme well suited for a hospital, general clinics, nursing home etc theme",
-        "link"      : "http://docs.sparklewpthemes.com/medicalheedpro/",
+        "link"      : "https://docs.sparklewpthemes.com/medicalheedpro/",
         "image"     : 'https://i0.wp.com/themes.svn.wordpress.org/medical-heed/1.0.2/screenshot.png?w=572&strip=all'
     },
     {
         "title" : "Kathmag Pro",
         "description" : "Kathmag is a simple and clean, ultra-fast free WordPress Magazine theme, Kathmag is specially designed for magazine, News portal, Blog etc",
-        "link"      : "http://docs.sparklewpthemes.com/kathmagpro/",
-        "image"     : 'http://docs.sparklewpthemes.com/kathmagpro/assets/img/banner.jpg'
+        "link"      : "https://docs.sparklewpthemes.com/kathmagpro/",
+        "image"     : 'https://docs.sparklewpthemes.com/kathmagpro/assets/img/banner.jpg'
     },
     {
         "title" : "Kingcabs",
         "description" : "Kingcabs is a responsive WordPress theme for Limousine, Limo service, Car hire company, Bus, Coach, Taxi, Cab, Airport transfer service",
-        "link"      : "http://docs.sparklewpthemes.com/kingcabs/",
-        "image"     : 'http://docs.sparklewpthemes.com/kingcabs/assets/images/kingcabs-cover-bg.png'
+        "link"      : "https://docs.sparklewpthemes.com/kingcabs/",
+        "image"     : 'https://docs.sparklewpthemes.com/kingcabs/assets/images/kingcabs-cover-bg.png'
     },
     {
         "title" : "Editorialmag",
         "description" : "Editorialmag is a user-friendly ultra-fast and cleans free WordPress magazine theme, specially designed for magazine, News portal, Blog and digital content publishing websites.",
-        "link"      : "http://docs.sparklewpthemes.com/editorialmag/",
-        "image"     : 'http://docs.sparklewpthemes.com/editorialmag/assets/images/editorialmag-cover-bg.jpg'
+        "link"      : "https://docs.sparklewpthemes.com/editorialmag/",
+        "image"     : 'https://docs.sparklewpthemes.com/editorialmag/assets/images/editorialmag-cover-bg.jpg'
     },
     {
         "title" : "Editorialmag Pro",
         "description" : "Editorialmag Pro is a user-friendly ultra-fast and cleans free WordPress magazine theme, specially designed for magazine, News portal, Blog and digital content publishing websites.",
-        "link"      : "http://docs.sparklewpthemes.com/editorialmagpro/",
-        "image"     : 'http://docs.sparklewpthemes.com/editorialmag/assets/images/editorialmag-cover-bg.jpg'
+        "link"      : "https://docs.sparklewpthemes.com/editorialmagpro/",
+        "image"     : 'https://docs.sparklewpthemes.com/editorialmag/assets/images/editorialmag-cover-bg.jpg'
     },
     {
         "title" : "BuzzStore",
         "description" : "BuzzStore is a clean, beautiful and fully customizable responsive WooCommerce Free WordPress theme",
-        "link"      : "http://docs.sparklewpthemes.com/buzzstore/",
-        "image"     : 'http://docs.sparklewpthemes.com/buzzstore/assets/images/buzzstore-cover-bg.jpg'
+        "link"      : "https://docs.sparklewpthemes.com/buzzstore/",
+        "image"     : 'https://docs.sparklewpthemes.com/buzzstore/assets/images/buzzstore-cover-bg.jpg'
     },
     {
         "title" : "BuzzStore Pro",
         "description" : "BuzzStore Pro is a clean, beautiful and fully customizable responsive WooCommerce Free WordPress theme",
-        "link"      : "http://docs.sparklewpthemes.com/buzzstorepro/",
-        "image"     : 'http://docs.sparklewpthemes.com/buzzstore/assets/images/buzzstore-cover-bg.jpg'
+        "link"      : "https://docs.sparklewpthemes.com/buzzstorepro/",
+        "image"     : 'https://docs.sparklewpthemes.com/buzzstore/assets/images/buzzstore-cover-bg.jpg'
     },
     {
         "title" : "MetroStore",
         "description" : "Metrostore is a creative, elegant multipurpose eCommerce/WooCommerce WordPress theme designed especially for WooCommerce shops, business/restaurants, health stores",
-        "link"      : "http://docs.sparklewpthemes.com/metrostore/",
-        "image"     : 'http://docs.sparklewpthemes.com/metrostore/assets/images/metrostore-cover-bg.jpg'
+        "link"      : "https://docs.sparklewpthemes.com/metrostore/",
+        "image"     : 'https://docs.sparklewpthemes.com/metrostore/assets/images/metrostore-cover-bg.jpg'
     },
     {
         "title" : "MetroStore Pro",
         "description" : "Metrostore Pro is a creative, elegant multipurpose eCommerce/WooCommerce WordPress theme designed especially for WooCommerce shops, business/restaurants, health stores",
-        "link"      : "http://docs.sparklewpthemes.com/metrostorepro/",
-        "image"     : 'http://docs.sparklewpthemes.com/metrostore/assets/images/metrostore-cover-bg.jpg'
+        "link"      : "https://docs.sparklewpthemes.com/metrostorepro/",
+        "image"     : 'https://docs.sparklewpthemes.com/metrostore/assets/images/metrostore-cover-bg.jpg'
     },
     {
         "title" : "SparkleStore",
         "description" : "Sparklestore is a clean, beautiful and fully customizable responsive modern free eCommerce/WooCommerce WordPress theme.",
-        "link"      : "http://docs.sparklewpthemes.com/sparklestore/",
-        "image"     : 'http://docs.sparklewpthemes.com/sparklestore/assets/images/sparklestore-cover-bg.jpg'
+        "link"      : "https://docs.sparklewpthemes.com/sparklestore/",
+        "image"     : 'https://docs.sparklewpthemes.com/sparklestore/assets/images/sparklestore-cover-bg.jpg'
     },
     {
         "title" : "SparkleStore Pro",
         "description" : "Sparklestore is a clean, beautiful and fully customizable responsive modern free eCommerce/WooCommerce WordPress theme.",
-        "link"      : "http://docs.sparklewpthemes.com/sparklestorepro/",
-        "image"     : 'http://docs.sparklewpthemes.com/sparklestore/assets/images/sparklestore-cover-bg.jpg'
+        "link"      : "https://docs.sparklewpthemes.com/sparklestorepro/",
+        "image"     : 'https://docs.sparklewpthemes.com/sparklestore/assets/images/sparklestore-cover-bg.jpg'
     }
 ];
 jQuery(document).ready(function($){
